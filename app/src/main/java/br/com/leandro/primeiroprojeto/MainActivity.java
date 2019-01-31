ca1a5886ac7a2e473745b83548ecf2a53cb4ddbb
Leandro de Almeida <leandroleekrap.gmail.com>
2019-01-31 18:11:58 -0200

    Criando meu primeiro App Android Java

---
+++ app/src/main/java/br/com/leandro/primeiroprojeto/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.leandro.primeiroprojeto;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TextView;
+
+        import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+        TextView textoResultado = (TextView) findViewById(R.id.textoResultado);
+            int numero = new Random().nextInt(11);
+        textoResultado.setText("O número selecionado é: "+ numero);
+    }
+
+}
